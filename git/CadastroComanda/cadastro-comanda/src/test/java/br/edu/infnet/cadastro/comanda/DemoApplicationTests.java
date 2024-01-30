d3c1780fda774e6673d239807ed62a0325f15dc0
Christianmed97 <82039439+Christianmed97@users.noreply.github.com>
2024-01-29 21:49:33 -0300

    Add files via upload

---
+++ git/CadastroComanda/cadastro-comanda/src/test/java/br/edu/infnet/cadastro/comanda/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.cadastro.comanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
