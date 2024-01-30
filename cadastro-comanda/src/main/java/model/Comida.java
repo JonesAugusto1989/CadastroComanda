663f1425723985543ed314acd05ae478ecb331f6
Christian Medeiros <Christianmedeiros2014@hotmail.com>
2024-01-29 21:48:17 -0300

    teste

---
+++ cadastro-comanda/src/main/java/model/Comida.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Comida{
+   private float peso;
+   private boolean vegano;
+   private String ingredientes;
+   
+   
+}
